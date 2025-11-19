e29218c45ce23057b79abb53426bd990a9e88aea
Lucas Caballero <caballerolucas18@gmail.com>
2025-11-19 16:50:56 -0300

    fetch use effect

--- src/components/Busqueda.tsx
+++ src/components/Busqueda.tsx
@@ -1,22 +1,42 @@
 import { useEffect, useState } from "react"
 
 interface Props{
-    data:string
+    name:[]
 }
+
 
 export const Busqueda = () => {
   
-    const  [data, setData] = useState('Hola')
+    const  [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const fecthData = async () =>{
+        try {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+            const jsonData = await response.json()
+
+            setData(jsonData)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     useEffect(()=>{
-        setData('holaa')
-        console.log(data)
+        fecthData()
     },[data])
   
-  
+    if (loading){
+        return <div> Cargando...</div>
+    }
+    
   
   
     return (
-    <div> {data} </div>
+        <div>
+
+            <div> {data.name} </div>
+            <div> {data.height} </div>
+            {/* <img src = {data.sprites.front_default} /> */}
+        </div>
+    
   )
 }
